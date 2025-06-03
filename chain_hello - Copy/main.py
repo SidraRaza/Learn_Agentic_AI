b9c4d8df04989f429e18722abb76c5504835398e
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-06-03 11:32:17 +0500

    update

--- chain_hello - Copy/main.py
+++ chain_hello - Copy/main.py
@@ -30,15 +30,24 @@ config = RunConfig(
 
 agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant", model=model)
 
+
+# message handler user list 
+@cl.on_chat_start
+async def start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="Maintain a conversation with me").send()
 @cl.on_message
 async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+    history.append({"role": "user", "content": message.content})
+    cl.user_session.set("history", history)
     result=await Runner.run(
         agent,
-        input=message.content,
+        input=history,
         run_config=config
     )
 
     await cl.Message(content=result.final_output).send()
-@cl.on_chat_start
-async def start():
-    await cl.Message(content="💡 *Made by Sidra Raza*").send()
+# @cl.on_chat_start
+# async def start():
+#     await cl.Message(content="💡 *Made by Sidra Raza*").send()
