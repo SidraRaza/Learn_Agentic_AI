3654b7a12cf6f7546f8fbaebf1b2c85d60cd87c1
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-06-08 12:45:46 +0500

    update

--- chain_hello - Copy/main.py
+++ chain_hello - Copy/main.py
@@ -11,7 +11,7 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
 
-#Reference: https://ai.google.dev/gemini-api/docs/openai
+
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
