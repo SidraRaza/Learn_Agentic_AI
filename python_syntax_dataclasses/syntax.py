05241e5459d4d04668e25523c10d13c383de7b55
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-06-11 07:55:34 +0500

    python_syntax_dataclasses

---
+++ python_syntax_dataclasses/syntax.py
@@ -0,0 +1,30 @@
+from typing import ClassVar
+from dataclasses import dataclass
+
+@dataclass
+class Pakistan:
+    name: str
+    age: int
+    weigth: float
+    language: ClassVar[str] = "Urdu"
+
+    def persons_name(self):
+        return f"My name is {self.name}"
+
+    def persons_age(self):
+        return self.age
+
+    @staticmethod
+    def country_language():
+        return Pakistan.language
+
+# Create an instance
+person = Pakistan(name="Sidra", age=20, weigth=50.5)
+
+# Print attributes
+print(person.name)          # Sidra
+print(person.age)           # 20
+print(person.weigth)        # 50.5
+
+# Call static method
+print(Pakistan.country_language())  # Urdu
